b4a47997935a6a9c111726e2eab2923a99774b2a
Jorge <jorgeperez99@yahoo.com>
2025-02-27 18:21:38 -0500

    made it more understandable

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CspUnsafeVal
 
 https://github.com/zvenigora/ng-eval?tab=readme-ov-file
+
+https://github.com/zvenigora/ng-eval 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,91 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterOutlet } from '@angular/router';
-import { ParserService } from '@zvenigora/ng-eval-core';
+import { ParserService, EvalService, CompilerService, DiscoveryService } from '@zvenigora/ng-eval-core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  private service!: ParserService;
+export class AppComponent implements OnInit {
+  expression: string | undefined;
+  result: any;
 
-  title = 'CspUnsafeVal';
-  safeEvalValue: string = '1 + foo';
-  useWindowEval: any;
+  constructor(
+    private parserService: ParserService,
+    private evalService: EvalService,
+    private compilerService: CompilerService,
+  ) { }
 
-  safeEval() {
-    this.service = new ParserService();
-    this.service.parse(this.safeEvalValue);
-    if (this.useWindowEval)
-      window.eval(this.safeEvalValue);
+  /**
+   * code from https://github.com/zvenigora/ng-eval
+   */
+  ngOnInit() {
+    // create ast
+    this.parsing();
+    // convert ast to function
+    this.compilation();
+    // executes the function
+    this.evaluation();
+  }
+
+  parsing() {
+    // parser
+    let expression = "2 + 3 * 5";
+    const ast = this.parserService.parse(expression);
+    console.log('ast: ', ast);
+  }
+
+  evaluation() {
+    // evaluation
+    const expression = '2 + 3 * a';
+    const context = { a: 10 };
+    this.result = this.evalService.simpleEval(expression, context); // 32
+    console.log(expression, this.result);
+  }
+
+  compilation() {
+    const expression = 'x + y';
+    const fn = this.compilerService.compile(expression);
+    const context = { x: 1, y: 2 };
+    const value = this.compilerService.simpleCall(fn, context); // 3
+    console.log(expression, value);
+  }
+
+  async evaluationAsync() {
+    // evaluation
+    const context = {
+      one: 1,
+      two: 2,
+      asyncFunc: async (a: number, b: number) => { return await (a+b); }
+    };
+    const expr = 'asyncFunc(one, two)';
+    this.result = await this.evalService.simpleEvalAsync(expr, context); // 32
+    console.log(this.result);
+  }
+
+  async compilationAsync() {
+    const context = {
+      one: 1,
+      two: 2,
+      promiseFunc: (a: number, b: number) => {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            return resolve(a + b);
+          }, 1000);
+        });
+      }
+    };
+    const expr = 'promiseFunc(one, two)';
+    const fn = this.compilerService.compileAsync(expr);
+    const result = await this.compilerService.simpleCallAsync(fn, context); // 3
+    console.log(result);
+  }
+
+  discovery() {
+    // discovery logic
+    console.log('Discovery method called');
   }
 }
