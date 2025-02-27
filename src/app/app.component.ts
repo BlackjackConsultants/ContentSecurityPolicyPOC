0c270ce2549ddc5accde84506fcb5b844ff8fb04
Jorge <jorgeperez99@yahoo.com>
2025-02-27 14:54:48 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+import { ParserService } from '@zvenigora/ng-eval-core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  private service!: ParserService;
+
   title = 'CspUnsafeVal';
+  safeEvalValue: string = '1 + foo';
+  useWindowEval: any;
+
+  safeEval() {
+    this.service = new ParserService();
+    this.service.parse(this.safeEvalValue);
+    if (this.useWindowEval)
+      window.eval(this.safeEvalValue);
+  }
 }
